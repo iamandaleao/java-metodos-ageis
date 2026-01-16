4ad5c867501f99d0c62586ddb29ba5a2e9819ed4
iamandaleao <amandabibiano144@gmail.com>
2026-01-16 17:34:12 -0300

    feat: create Car class and test methods

---
+++ src/main/java/issue2/Carro.java
@@ -0,0 +1,11 @@
+package issue2;
+
+public class Carro {
+    String modelo;
+    int ano;
+
+    public Carro(String modelo, int ano) {
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+}
